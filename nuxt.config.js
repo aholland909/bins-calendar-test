75be1b6337a6fabe364110f200f8076e01a8c631
aholland909 <andrewholland909@gmail.com>
2020-07-08 16:26:57 +0100

    Removed auth

--- nuxt.config.js
+++ nuxt.config.js
@@ -57,7 +57,6 @@ export default {
   },
   router: {
     // middleware: 'redirect'
-    middleware: 'authenticated'
   },
   /*
   ** Build configuration
--- store/index.js
+++
@@ -1,9 +0,0 @@
-export const state = () => ({
-    authenticated: false
-})
-
-export const mutations = {
-    auth(state) {
-        state.authenticated = true
-    }
-}
