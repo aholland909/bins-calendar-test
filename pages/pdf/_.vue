53d3165b1a3b0b2570bb89179b17bcea531693ce
aholland909 <andrewholland909@gmail.com>
2021-01-11 12:25:02 +0000

    Removed test date methods

--- pages/pdf/_.vue
+++ pages/pdf/_.vue
@@ -56,7 +56,7 @@ export default {
     Calendar,
   },
   mounted() {
-    // this.loadingComponent = this.$buefy.loading.open(); //disable for testing
+    this.loadingComponent = this.$buefy.loading.open(); //disable for testing
     var pathArray = this.path.split("/");
     this.uprn = pathArray[0];
     this.postcode = pathArray[1];
@@ -73,8 +73,8 @@ export default {
     },
     domRendered() {
       console.log("Cal Has Rendered");
-      // this.isLoading = false; //disable for testing
-      // this.loadingComponent.close(); //disable for testing
+      this.isLoading = false; //disable for testing
+      this.loadingComponent.close(); //disable for testing
 
       // setTimeout(() => {
       //       if (process.browser) {
